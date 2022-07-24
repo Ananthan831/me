3752a3fc368245aebdaee10026f2377c8a9966b9
Ananthan831 <ananthananil8301@gmail.com>
2022-07-24 09:18:16 +0530

    Ananthan

--- js1.js
+++ js1.js
@@ -1,14 +1,25 @@
- $(document).ready(function(){
-    $('#menu').click(function(){
-        $(this).toggleClass('fa-times');
-        $('header').toggleClass('toggle');
-    });
- });
- $(window).on('scroll load',function(){
-    $('#menu').removeClass('fa-times');
-    $('header').removeClass('toggle');
- });
- let yellow=
+$(document).ready(function(){
+   $('#menu').click(function(){
+      $(this).toggleClass('fa-times');
+      $('header').toggleClass('toggle');
+   });
+   
+   $(window).on('scroll load',function(){
+      $('#menu').removeClass('fa-times');
+      $('header').removeClass('toggle');
+   });
+   
+   $('a[href*="#]') .on('click',function(e){
+      e.preventDefult();
+      $('html,body').animate({
+         scrollTop:$($(this).attr('href')).offset().top,
+      },
+        500,
+        'linear'
+      );
+   });
+}); 
+let yellow=
  document.getElementById('yellow')
  yellow.addEventListener('change',(e)=>{
 
