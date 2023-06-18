af312d69e2f57d94bc6b074cf68caae46862a320
Ananthan831 <107261363+Ananthan831@users.noreply.github.com>
2023-06-18 16:01:45 +0530

    Add files via upload

---
+++ JS BASIC.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    $('#menu').click(function(){
+       $(this).toggleClass('fa-times');
+       $('header').toggleClass('toggle');
+    });
+    
+    $(window).on('scroll load',function(){
+       $('#menu').removeClass('fa-times');
+       $('header').removeClass('toggle');
+    });
+    
+    $('a[href*="#"]') .on('click',function(e){
+       e.preventDefault();
+       $('html,body').animate({
+          scrollTop: $($(this).attr('href')).offset().top,
+       },
+         500,
+         'linear'
+       );
+    });
+ }); 
+ let yellow=
+  document.getElementById('yellow')
+  yellow.addEventListener('change',(e)=>{
+    document.documentElement.style.setProperty('--yellow',e.target.value)
+ })
+ let blue=
+  document.getElementById('blue')
+  blue.addEventListener('change',(e)=>{
+    document.documentElement.style.setProperty('--blue',e.target.value)
+ })
+ 
+ 
+ 
+ 
+ document
+   .querySelector("#contact")
+   .addEventListener("submit", handleSubmit);
+ 
+ const handleSubmit = (e) => {
+   e.preventDefault();
+   let myForm = document.getElementById("contact");
+   let formData = new FormData(myForm);
+   console.log(myForm)
+   fetch("/", {
+     method: "POST",
+     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+     body: new URLSearchParams(formData).toString(),
+   })
+     .then(() => console.log("Form successfully submitted"))
+     .catch((error) => alert(error));
+ };
